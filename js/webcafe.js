3978b97da5702164c12263f57fe889e6ea119d81
seulbinim <seulbinim@gmail.com>
2018-04-02 18:49:50 +0900

    비디오 제어를 위한 자바스크립트 적용

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,26 +1,31 @@
-// var container = document.querySelector('.container');
-// var grid = document.querySelector('.btn-grid');
-
-// grid.addEventListener('click', function(){
-//     container.classList.toggle('is-act');
-// });
-
-// $(document).ready(function(){
-//     var video = $('.news-video');
-
-//     video.on('mouseover focusin', function(){
-//         $(this).attr('autoplay', 'true');
-//     });
-//     video.on('mouseout focusout', function(){
-//         $(this).attr('autoplay', 'false');
-//     });
-// });
-
-var video = document.querySelector('.news-video');
-
-video.addEventListener('mouseenter focusin', function(){
-    this.setAttribute('autoplay');
+// jQuery
+// autoplay 속성은 페이지 로딩 시점에서 자동 시작하기때문에
+// video 관련 API를 이용하여 재생과 정지를 제어할 수 있음.
+$(document).ready(function(){
+    var container = $('.container');
+    var grid = document.querySelector('.btn-grid');
+    var video = $('.news-video');
+    grid.on('click', function(){
+        container.toggleClass('is-act');
+    });
+    video.on('mouseover focusin', function(){
+        this.play();
+    });
+    video.on('mouseout focusout', function(){
+        this.pause();
+    });
 });
-video.addEventListener('mouseleave focusout', function(){
-    this.removeAttribute('autoplay');
-});
+// Javascript
+// var video = document.querySelector('.news-video');
+// var eventOn = ['mouseenter', 'focusin'];
+// var eventOff = ['mouseleave', 'focusout'];
+// for(event of eventOn){
+//     video.addEventListener(event, function(){
+//         video.play();
+//     });
+// }
+// for(event of eventOff){
+//     video.addEventListener(event, function(){
+//         video.pause();
+//     });
+// }

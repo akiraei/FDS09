7b0bfcb19ff844ab4ffef78be71ea76aa66e5099
seulbinim <seulbinim@gmail.com>
2018-04-02 17:09:44 +0900

    video 제어를 위한 자바스크립트

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,6 +1,26 @@
-var container = document.querySelector('.container');
-var grid = document.querySelector('.btn-grid');
+// var container = document.querySelector('.container');
+// var grid = document.querySelector('.btn-grid');
 
-grid.addEventListener('click', function(){
-    container.classList.toggle('is-act');
+// grid.addEventListener('click', function(){
+//     container.classList.toggle('is-act');
+// });
+
+// $(document).ready(function(){
+//     var video = $('.news-video');
+
+//     video.on('mouseover focusin', function(){
+//         $(this).attr('autoplay', 'true');
+//     });
+//     video.on('mouseout focusout', function(){
+//         $(this).attr('autoplay', 'false');
+//     });
+// });
+
+var video = document.querySelector('.news-video');
+
+video.addEventListener('mouseenter focusin', function(){
+    this.setAttribute('autoplay');
+});
+video.addEventListener('mouseleave focusout', function(){
+    this.removeAttribute('autoplay');
 });

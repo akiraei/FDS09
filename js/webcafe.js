d523cbb87f3ee9bdc6b841d68c3f170ca72209d7
seulbinim <seulbinim@gmail.com>
2018-04-05 15:55:30 +0900

    메인메뉴 제어를 위한 스크립트

--- js/webcafe.js
+++ js/webcafe.js
@@ -2,11 +2,25 @@ $(document).ready(function(){
     var nav = $('.navigation');
     var btn = $('.btn-menubar');
     var span = $('.menubar'); 
+    var menu = $('.menu');
+    var menuLists = $('.menu-list');
+    var menuItems = $('.menu-item');
+    var subMenus = $('.sub-menu');
     var video = $('.news-video');
+
     btn.on('click', function(){
         nav.toggleClass('is-act');
+        if(nav.hasClass('is-act')){
+            btn.attr('aria-label', '메인메뉴 닫기');
+        }else{
+            btn.attr('aria-label', '메인메뉴 열기');
+        }
     })
 
+    menuItems.on('click', function(){
+        $(this).parent().addClass('menu-act');
+        $(this).siblings().css('display', 'block');
+    });
 
     video.get(0).volume = 0.0;
 

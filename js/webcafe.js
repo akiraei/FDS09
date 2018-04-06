2feab19b9a5a3fa920c264cf6a8ec6e2e0787590
seulbinim <seulbinim@gmail.com>
2018-04-06 11:49:34 +0900

    메인 메뉴에서 focusout시 메뉴가 숨겨지도록 스크립트 추가

--- js/webcafe.js
+++ js/webcafe.js
@@ -7,13 +7,14 @@ $(document).ready(function () {
       var menuLists = $('.menu-list');
       var menuItems = $('.menu-item');
       var subMenus = $('.sub-menu');
-
+      var slogan = $('.slogan-heading');
       // 999px 이하에서 적용할 코드
       menuItems.attr('aria-haspopup', 'true');
       menuItems.attr('aria-expanded', 'false');
       menuItems.attr('role', 'button');
       menuItems.attr('tabindex', '0');
       menuItems.addClass('icon-plus');
+      slogan.addClass('a11y-hidden');
 
       btn.on('click', function(e) {
         e.preventDefault();
@@ -26,21 +27,30 @@ $(document).ready(function () {
       });
   
       menuItems.on('click keydown', function(e) {
-        if (e.type === 'click' || (e.type === 'keydown' && e.keyCode === 13)) {
-          menuLists.removeClass('is-act');
-          menuItems.addClass('icon-plus');
-          subMenus.css('display', 'none');
-          $(this).parent().addClass('menu-act');
-          if ($(this).parent().hasClass('menu-act')) {
-            $(this).attr('aria-expanded', 'true');
-            $(this).addClass('icon-minus');
-            $(this).removeClass('icon-plus');
-            $(this).siblings().css('display', 'block');
-          } else {
-            $(this).attr('aria-expanded', 'false');
-          }
+        if(e.type === 'click' || (e.type === 'keydown' && e.keyCode === 13)){
+            menuLists.removeClass('menu-act');
+            menuItems.addClass('icon-plus');
+            subMenus.css('display', 'none');
+            menuItems.attr('tabindex', '0');
+            $(this).parent().addClass('menu-act');
+            if($(this).parent().hasClass('menu-act')){
+                $(this).addClass('icon-minus');
+                $(this).removeClass('icon-plus');
+                $(this).attr('aria-expanded', 'true');
+                $(this).attr('aria-haspopup', 'true');
+                $(this).siblings().css('display', 'block');
+            }else{
+                $(this).attr('aria-expanded', 'false');
+            }
         }
       });
+      menu.on('focusout', function(){
+          nav.removeClass('is-act');
+      });
+      menu.on('focusin', function(){
+            nav.addClass('is-act');
+        });
+
     } else {
       // 1000px 이상에서 사용할 코드
     }
